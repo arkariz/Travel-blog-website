04d68530611378b239d9edeccee2b38bf3063477
Muhammad Rizky <44420394+MHDRizky0@users.noreply.github.com>
2020-07-30 19:29:23 +0700

    Update settings.py
    fix added allowed host heroku

--- travel_blog/settings.py
+++ travel_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9d0vvi*izx@xt_xkr!e_usxs7fr@k8j7912xlr+-5-)8m)@b7^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['powerful-sands-49232.herokuapp.com', '126.0.0.1']
+ALLOWED_HOSTS = ['powerful-sands-49232.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
